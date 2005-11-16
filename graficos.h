6bce0952d52f7ca94879d5abd63e445795cfbed3
necroromancist <necroromancist@localhost>
2005-11-16 23:57:18 +0000

    Just a change to make it compile better on linux

--- graficos.h
+++ graficos.h
@@ -51,7 +51,7 @@ void cb_isp_backgnd_t(DWORD addr, void * p, size_t size);
 
 // i really don't know why i need to export this in Linux and in Dev C++ it works oh well
 
-#if defined (__GNUC__)
+#if defined (POSX)
 void cb_renderstart(DWORD addr, void * p, size_t size);
 void cb_param_base(DWORD addr, void * p, size_t size);
 void cb_region_base(DWORD addr, void * p, size_t size);
