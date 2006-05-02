20f1da773dfd8063b1ef18a151879133919462b2
itoledo <itoledo@birdie.cl>
2006-05-02 13:36:55 +0000

    *** empty log message ***

--- main.c
+++ main.c
@@ -866,9 +866,10 @@ int main(int argc, char *argv[])
 	init_cpu_jumptable();
 #endif
 //  	PC = mem_base + ip_bs1_offset; // ip_bs1_offset; // + mem_offset;
- 	PC = 0x8c010000;
+// 	PC = 0x8c010000;
 //	PC = 0x00000000;
 //	PC = 0x8c0000e0;
+    PC = 0x8C008300;
 
 //	str_PC = get_memory_pointer(PC);
 	
